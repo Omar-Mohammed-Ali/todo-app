230c65e3dd7271bf6a045c46a59307e64d3922a3
Omar-Mohammed-Ali <omarmohammed2224@gmail.com>
2026-01-22 13:51:22 +0300

    complete feature and pop

--- web.py
+++ web.py
@@ -12,10 +12,13 @@ todos = functions.get_todos()
 st.title("My Todo App")
 st.subheader("This is my todo app")
 
-for todo in todos:
-    st.checkbox(todo)
+for index, todo in enumerate(todos):
+    checkbox = st.checkbox(todo,key=todo)
+    if checkbox:
+        todos.pop(index)
+        functions.write_todos(todos)
+        del st.session_state[todo]
+        st.rerun()
 
 st.text_input(label=" ",placeholder="Add new todo...",
               on_change=add_todo,key="new_todo")
-
-st.session_state
